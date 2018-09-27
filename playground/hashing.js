7e453c0263ecc00fb327281327a81970763bdcc7
Francisco Vargas <fjvarg4934@gmail.com>
2018-09-27 09:19:07 -0500

    Add POST /users/login route

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$MzI0mWfrzN7MnnTdtHRiA.io3LN3KUlemPb5/pfbb.jmBlRlElLku';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -35,4 +50,4 @@ console.log('decoded', decoded);
 // } else {
 //   console.log('Data was changed. Do not trust.');
 // }
-user.tokens = user.tokens.concat([{access, token}]);
+// user.tokens = user.tokens.concat([{access, token}]);
