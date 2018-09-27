fb5e1e8712a9290091404448c6bf06cc4c7d836d
Francisco Vargas <fjvarg4934@gmail.com>
2018-09-27 07:44:50 -0500

    GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -23,14 +24,14 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({todos});
   }, (e) => {
-    res.status(400),send(e);
+    res.status(400).send(e);
   });
 });
 
@@ -38,12 +39,12 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    return res.status(400).send({});
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(400).send()
+      return res.status(404).send();
     }
 
     res.send({todo});
@@ -94,6 +95,24 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
